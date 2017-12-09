af569e705d3ce93d676d74d311ce1e0470fd3fdb
Matt Dayton <jmattdayton@gmail.com>
2017-12-09 10:11:07 -0800

    Update to AutoOption to make toggle wait for button release.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoOption.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoOption.java
@@ -14,8 +14,22 @@ public class AutoOption extends AbstractCougarRobot {
 
         while (! isStarted()) {
 
-            if (gamepad1.a) isRed = ! isRed;
-            if (gamepad1.y) isFront = ! isFront;
+            if (gamepad1.a) {
+                isRed = ! isRed;
+                while(gamepad1.a) {
+                    //wait here for the button to be released
+                    telemetry.addData("Alliance (a)", isRed ? "BLUE" : "RED");
+                    telemetry.update();
+                }
+            }
+            if (gamepad1.y) {
+                isFront = ! isFront;
+                while(gamepad1.a) {
+                    //wait here for the button to be released
+                    telemetry.addData("Alliance (a)", isFront ? "BLUE" : "RED");
+                    telemetry.update();
+                }
+            }
             telemetry.addData("Alliance (a)", isRed ? "BLUE" : "RED");
             telemetry.addData("Position (y)", isFront ? "BACK" : "FRONT");
             telemetry.addData("Time", getRuntime());
